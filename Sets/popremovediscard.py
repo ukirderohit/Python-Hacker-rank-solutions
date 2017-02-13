f19c16ecb36121080b524bdbe4654b1b9eb5274f
Rohit Ukirde <ukirderohit@gmail.com>
2017-02-12 23:44:42 -0600

    sets

--- Sets/popremovediscard.py
+++ Sets/popremovediscard.py
@@ -1,15 +1,7 @@
-n = int(raw_input())
-s = set(map(int, raw_input().split()))
-c = int(raw_input())
-
-for i in range(c):
-    command = input().split()
-    
-    if command[0] == 'pop':
-        s.pop()
-    elif command[0] == 'remove':
-        s.remove(int(command[1]))
-    elif command[0] == 'discard':
-        s.discard(int(command[1]))
-
+n = input()
+s = set(map(int, raw_input().split())) 
+N = int(input())
+for i in range(N):
+    cmd = (raw_input() + " ").split(" ")
+    eval('s.' + cmd[0] + '(' + cmd[1] + ')')
 print(sum(s))
